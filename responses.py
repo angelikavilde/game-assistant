fad286338d590209d9c5e33c2fd028217261f258
Angela <angelikavilde@gmail.com>
2023-07-20 22:44:08 +0100

    code cleaned

--- responses.py
+++ responses.py
@@ -1,15 +1,7 @@
-import random
-
-
-def handle_response(message) -> str:
-    p_message = message.lower()
-    if p_message == 'hello':
+def handle_response(message):
+    msg = message.lower()
+    if msg == 'hello':
         return 'Hey there!'
 
-    if p_message == 'roll':
-        return str(random.randint(1, 6))
-
-    if p_message == '!help':
-        return "`This is a help message that you can modify.`"
-
-    #  return 'Yeah, I don\'t know. Try typing "!help".'
+    if msg == 'help':
+        return "`I help!`"
