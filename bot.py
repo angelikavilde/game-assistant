79c544a6efbad90c6bb6d4392f7e1d4c93d42fa5
Angela <angelikavilde@gmail.com>
2023-07-21 19:25:50 +0100

    codenames event started

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 """File that executes our bots functions"""
 
 import time
+import random
 
 import discord
 import requests
@@ -8,7 +9,8 @@ from dotenv import dotenv_values
 
 import responses
 
-
+codename_event = False
+users_playing = []
 async def send_message(message, user_message: str, user: str) -> None:
     """Sends an appropriate response to a query sent with '!' """
     try:
@@ -43,10 +45,25 @@ def run_discord_bot():
 
         print(f"{user} said: '{msg}' ({chnl})")
 
-        greetings = ["hi","hello","hey","hola"]
-        bot = ["ga","gameassist"]
+        greetings = ["hi", "hello", "hey", "hola"]
+        bot = ["ga", "gameassist", "bot"]
 
-        if msg == "!joke":
+        if msg[:6] == "!play ":
+            match msg[6:]:
+                case "codenames":
+                    global codename_event
+                    if codename_event:
+                        await message.channel.send("`CodeNames event is already running!`")
+                    else:
+                        codename_event = True
+                        response = start_codenames(msg,user)
+                        await handle_events(message, response)
+        elif msg[0] == "/":
+            global codename_event
+            if codename_event:
+                response = start_codenames(msg,user)
+                await handle_events(message, response)
+        elif msg == "!joke":
             await send_joke(message)
         elif any(word in msg for word in greetings) and any(word in msg for word in bot):
             await send_message(message, msg, user)
@@ -58,6 +75,7 @@ def run_discord_bot():
 
     client.run(TOKEN)
 
+
 async def send_joke(message) -> None:
     request = requests.get("https://official-joke-api.appspot.com/jokes/programming/random")
     joke = request.json()
@@ -65,3 +83,59 @@ async def send_joke(message) -> None:
     await message.channel.send(joke[0]["setup"])
     time.sleep(int(len(joke[0]["setup"])/10))
     await message.channel.send(joke[0]["punchline"])
+
+
+async def start_among_us():
+
+    #shuts off automatically if not used
+    time.sleep(2400)
+    """"""
+
+
+async def handle_events(message, msg: str):
+    """Events when activated allow to run '/' commands.
+    Some might however not exist."""
+    try:
+        await message.channel.send(msg)
+    except Exception as e:
+        print(e)
+        await message.channel.send("This is not an allowed command for this event. Try '/h' for help!")
+
+
+#starts an event which allows to add yourself to it and it'll print out the teams.
+def start_codenames(user_chose: str, user: str) -> str:
+    global codename_event
+    global users_playing
+    if user_chose[:3] == "/d ":
+        """Deletes a user from the game who did not leave by themselves"""
+        user = user_chose[3:]
+        user_chose = "/l"
+    if user_chose == "/j":
+        """Joins the user to the game"""
+        users_playing.append(user)
+        return f"{user} was successfully added to the CodeNames event!"
+    if user_chose == "/l":
+        """User leaves the game"""
+        if user in users_playing:
+            users_playing.remove(user)
+            return f"`{user} successfully left CodeNames event!`"
+        else:
+            return f"`{user} user did not enter the game. '/j' to join! `"
+    if user_chose == "/s":
+        """Game is started"""
+        if len(users_playing) < 4:
+            return "`This game is designed for minimum of 4 people. Preferably min 6. Add more players!`"
+        users_joined = users_playing.copy()
+        team2 = []
+        for _ in range(int(len(users_playing)/2)):
+            team2.append(users_joined.pop(users_joined.index(random.choice(users_joined))))
+        teams = f"""``` Team 1: {users_joined}; Captain: {random.choice(users_joined)}
+                    Team 2: {team2}; Captain: {random.choice(team2)}```"""
+        return teams
+    if user_chose == "/h":
+        #return help about this event
+        return "f"
+    if user_chose == "/q":
+        codename_event = False
+        users_playing = []
+        return "`CodeNames event was ended!`"
