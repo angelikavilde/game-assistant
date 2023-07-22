cee2523226cb64fe1fde5bee09ba41c9abb62725
Angela <angelikavilde@gmail.com>
2023-07-22 16:34:37 +0100

    codenames event rewritten and works except for the timout task that was made to stop the game when not ran, still work in progress

--- bot.py
+++ bot.py
@@ -5,12 +5,15 @@ import random
 
 import discord
 import requests
+import asyncio
 from dotenv import dotenv_values
 
 import responses
 
 codename_event = False
+timeout_task = 0
 users_playing = []
+
 async def send_message(message, user_message: str, user: str) -> None:
     """Sends an appropriate response to a query sent with '!' """
     try:
@@ -18,11 +21,12 @@ async def send_message(message, user_message: str, user: str) -> None:
         await message.channel.send(response)
     except Exception as e:
         print(e)
-        #await message.channel.send("Sorry, I do not currently have a response for you.")
+        await message.channel.send("Sorry, I do not currently have a response for you.")
 
 
 def run_discord_bot():
-    """Function that runs the bot with provided key and """
+    """Function that runs the bot with provided key,
+    and listens to messages."""
     config = dotenv_values('.env')
     TOKEN = config["TOKEN"]
     intents = discord.Intents.default()
@@ -36,9 +40,11 @@ def run_discord_bot():
 
     @client.event
     async def on_message(message):
+        global codename_event, timeout_task
         # No response if it was a message by our bot
         if message.author == client.user:
             return
+
         user = str(message.author)
         msg = str(message.content).lower()
         chnl = str(message.channel)
@@ -46,26 +52,26 @@ def run_discord_bot():
         print(f"{user} said: '{msg}' ({chnl})")
 
         greetings = ["hi", "hello", "hey", "hola"]
-        bot = ["ga", "gameassist", "bot"]
+        bot_names = ["ga", "gameassist", "bot"]
 
         if msg[:6] == "!play ":
+            #TODO make sure timeout task is cancelled if no appropriate game was chosen
+            timeout_task = asyncio.create_task(timeout(message))
             match msg[6:]:
                 case "codenames":
-                    global codename_event
                     if codename_event:
                         await message.channel.send("`CodeNames event is already running!`")
                     else:
                         codename_event = True
-                        response = start_codenames(msg,user)
-                        await handle_events(message, response)
-        elif msg[0] == "/":
-            global codename_event
+                        await message.channel.send("`CodeNames event was started!`")
+        elif msg[0:2] == "//":
             if codename_event:
-                response = start_codenames(msg,user)
-                await handle_events(message, response)
+                response = start_codenames(message, msg,user)
+                await handle_event_responses(message, response)
+                await message.delete()
         elif msg == "!joke":
             await send_joke(message)
-        elif any(word in msg for word in greetings) and any(word in msg for word in bot):
+        elif any(word in msg for word in greetings) and any(word in msg for word in bot_names):
             await send_message(message, msg, user)
         elif msg[0] == "!":
             await send_message(message, msg[1:], user)
@@ -85,14 +91,7 @@ async def send_joke(message) -> None:
     await message.channel.send(joke[0]["punchline"])
 
 
-async def start_among_us():
-
-    #shuts off automatically if not used
-    time.sleep(2400)
-    """"""
-
-
-async def handle_events(message, msg: str):
+async def handle_event_responses(message, msg: str):
     """Events when activated allow to run '/' commands.
     Some might however not exist."""
     try:
@@ -102,40 +101,58 @@ async def handle_events(message, msg: str):
         await message.channel.send("This is not an allowed command for this event. Try '/h' for help!")
 
 
+async def timeout(message):
+    await asyncio.sleep(20)
+    response =  start_codenames(message, "//q", "timer")
+    print("finished")
+    await message.channel.send(response)
+
+
 #starts an event which allows to add yourself to it and it'll print out the teams.
-def start_codenames(user_chose: str, user: str) -> str:
-    global codename_event
-    global users_playing
-    if user_chose[:3] == "/d ":
-        """Deletes a user from the game who did not leave by themselves"""
-        user = user_chose[3:]
-        user_chose = "/l"
-    if user_chose == "/j":
-        """Joins the user to the game"""
+def start_codenames(message, user_chose: str, user: str) -> str:
+    global codename_event, users_playing, timeout_task
+    timeout_task.cancel()
+    timeout_task = asyncio.create_task(timeout(message))
+    if user_chose[:4] == "//d ":
+        # allows to delete another user from the game
+        user = user_chose[4:]
+        user_chose = "//l"
+    if user_chose == "//j":
+        # adds the user to the game
+        if user in users_playing:
+            return f"`{user} has already joined the CodeNames event!`"
         users_playing.append(user)
-        return f"{user} was successfully added to the CodeNames event!"
-    if user_chose == "/l":
-        """User leaves the game"""
+        return f"`{user} was successfully added to the CodeNames event!`"
+    if user_chose == "//l":
+        # user leaves the game
         if user in users_playing:
             users_playing.remove(user)
             return f"`{user} successfully left CodeNames event!`"
         else:
-            return f"`{user} user did not enter the game. '/j' to join! `"
-    if user_chose == "/s":
-        """Game is started"""
+            return f"`{user} did not enter the game. '//j' to join!`"
+    if user_chose == "//s":
+        # game is started
         if len(users_playing) < 4:
-            return "`This game is designed for minimum of 4 people. Preferably min 6. Add more players!`"
-        users_joined = users_playing.copy()
+            return "`This game is designed for minimum of 4 people. Preferably 6 or more. Add more players!`"
+        team1 = users_playing.copy()
         team2 = []
         for _ in range(int(len(users_playing)/2)):
-            team2.append(users_joined.pop(users_joined.index(random.choice(users_joined))))
-        teams = f"""``` Team 1: {users_joined}; Captain: {random.choice(users_joined)}
-                    Team 2: {team2}; Captain: {random.choice(team2)}```"""
+            team2.append(team1.pop(team1.index(random.choice(team1))))
+        teams = f"""``` Team 1: {team1}; Captain: {random.choice(team1)}
+ Team 2: {team2}; Captain: {random.choice(team2)}```"""
         return teams
-    if user_chose == "/h":
-        #return help about this event
-        return "f"
-    if user_chose == "/q":
+    if user_chose == "//h":
+        # return help about this event
+        return "-"
+    if user_chose == "//q":
+        # finishes the game
         codename_event = False
         users_playing = []
         return "`CodeNames event was ended!`"
+
+
+async def start_among_us():
+
+    #shuts off automatically if not used
+    time.sleep(2400)
+    """"""
