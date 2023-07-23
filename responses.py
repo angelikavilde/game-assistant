bba8f42a1ac62db68886fa0af5e63454525d437d
Angela <angelikavilde@gmail.com>
2023-07-23 21:26:55 +0100

    greeting was taken out

--- responses.py
+++ responses.py
@@ -1,9 +1,9 @@
 
-def handle_response(msg: str, user: str) -> str:
+def handle_response(msg: str) -> str:
     """Function that determines what to do with the responses from user"""
-    if any(word in msg for word in ["hi","hello","hey","hola"]):
-        return f"""```json
-            "Hi {user} <3"
-            ```"""
+
     if msg == "help":
         return "`I help!`"
+    if msg[:7] == "repeat ":
+        return msg[7:]
+
