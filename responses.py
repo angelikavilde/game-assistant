28ddf8c0b8698df692aad6092d8f49c8644c647e
Angela <angelikavilde@gmail.com>
2023-08-01 13:01:41 +0100

    help message added to responses.py

--- responses.py
+++ responses.py
@@ -2,8 +2,13 @@
 def handle_response(msg: str) -> str:
     """Function that determines what to do with the responses from user"""
 
-    if msg == "help":
-        return "`I help!`"
+    if msg == "help" or msg == "h":
+        return """ **I am in the testing mode so I do not currently run all of the below**
+These are _some_ of the commands I respond to:
+* !repeat - repeats a message after you and deletes your sent message
+* !joke - sends programmer joke
+* !play x - runs an event x. List of available events: rps, codenames, amongus.
+**each event when activated has its own commands //h for help about the event**
+* !kill - deletes last bots message and the command will be deleted off the screen too (!kill)"""
     if msg[:7] == "repeat ":
         return msg[7:]
-
