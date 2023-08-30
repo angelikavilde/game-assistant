c37a1d98573339384401d381404e85eb1d74210f
angelikavilde <angelikavilde@gmail.com>
2023-08-30 21:25:26 +0100

    changed formatting

--- events_help.py
+++ events_help.py
@@ -2,6 +2,7 @@
 
 def help_documentation(event: str):
     """Returns formatted string of documentation for an event"""
+
     match event:
         case "dnd":
             return """
@@ -15,13 +16,16 @@ __DnD event:__
 * //add magic x - Adds a magic item following this format:
 ```
 x =  item_name, item_type, rarity, attunement_req, item_class, description
+
 attunement_req can only be yes/no, if yes - mention class (if no, put "-" for item class)
+
 Item type must be a number! See below:
-1 - 'Armour', 2 -'Potion', 3 - 'Ring', 4 - 'Rod', 5 - 'Scroll', 6 - 'Staff',
-7 - 'Wand', 8 - 'Weapon', 9 - 'Wondrous Item'
-Rarity must be a number! See below":
-1 - 'Common', 2 - 'Uncommon', 3 - 'Rare', 4 - 'Very Rare', 5 - 'Legendary',
-6 - 'Artifact', 7 - 'Varies', 8 - 'Unknown Rarity'
+1 - Armour, 2 -Potion, 3 - Ring, 4 - Rod, 5 - Scroll,
+6 - Staff, 7 - Wand, 8 - Weapon, 9 - Wondrous Item
+
+Rarity must be a number! See below:
+1 - Common, 2 - Uncommon, 3 - Rare, 4 - Very Rare, 5 - Legendary,
+6 - Artifact, 7 - Varies, 8 - Unknown Rarity
 ``` 
 * //q - Quits the event
 **DnD event is associated with database live on cloud so answer's wait-time might vary**
