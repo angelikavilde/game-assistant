3c172b07a7d2a94c505a4851cf0cb0ae796c6e34
angelikavilde <angelikavilde@gmail.com>
2023-09-11 21:27:45 +0100

    changed events variable to a BotEvents object type

--- codenames.py
+++ codenames.py
@@ -3,9 +3,10 @@
 import random
 
 from events_help import help_documentation
+from bot import BotEvents
 
 
-def start_codenames(user_chose: str, user: str, events: dict) -> tuple[str,dict]:
+def start_codenames(user_chose: str, user: str, events: BotEvents) -> str:
     """CodeNames event function"""
 
     if user_chose[:4] == "//d ":
@@ -13,34 +14,34 @@ def start_codenames(user_chose: str, user: str, events: dict) -> tuple[str,dict]
         user_chose = "//l"
 
     if user_chose == "//j":
-        if user in events["users_playing"]:
-            return f"`Cannot join! {user} has already joined the CodeNames event!`", events
-        events["users_playing"].append(user)
-        return f"`{user} was successfully added to the CodeNames event!`", events
+        if user in events.users_playing:
+            return f"`Cannot join! {user} has already joined the CodeNames event!`"
+        events.users_playing.append(user)
+        return f"`{user} was successfully added to the CodeNames event!`"
 
     if user_chose == "//l":
-        if user in events["users_playing"]:
-            events["users_playing"].remove(user)
-            return f"`{user} successfully left CodeNames event!`", events
+        if user in events.users_playing:
+            events.users_playing.remove(user)
+            return f"`{user} successfully left CodeNames event!`"
         else:
-            return f"`Cannot leave! {user} did not enter the game. '//j' to join!`", events
+            return f"`Cannot leave! {user} did not enter the game. '//j' to join!`"
 
     if user_chose == "//t":
-        if len(events["users_playing"]) < 4:
-            return "`This game is designed for minimum of 4 people. Preferably 6 or more. Add more players!`", events
+        if len(events.users_playing) < 4:
+            return "`This game is designed for minimum of 4 people. Preferably 6 or more. Add more players!`"
         team1 = events["users_playing"].copy()
         team2 = []
         for _ in range(int(len(events["users_playing"])/2)):
             team2.append(team1.pop(team1.index(random.choice(team1))))
         teams = f"""```Team 1: {team1}; Captain: {random.choice(team1)}
 Team 2: {team2}; Captain: {random.choice(team2)}```"""
-        return teams, events
+        return teams
 
     if user_chose == "//h":
-        return help_documentation("codenames"), events
+        return help_documentation("codenames")
 
     if user_chose == "//q":
-        events["codename_event"] = False
-        events["users_playing"] = []
-        return "`CodeNames event was ended!`", events
-    return "", events
+        events.codenames_event = False
+        events.users_playing = []
+        return "`CodeNames event was ended!`"
+    return ""
