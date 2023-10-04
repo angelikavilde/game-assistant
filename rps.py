d31be8893f03f47e400c2765b39b09d6f1930703
angelikavilde <angelikavilde@gmail.com>
2023-10-04 21:43:46 +0100

    fixed rps not running as intended

--- rps.py
+++ rps.py
@@ -5,11 +5,12 @@ from discord.message import Message
 from discord.ui import button, Button, View
 
 
-async def check_rps_played(interaction: Interaction, bot_message: Message) -> bool:
+async def check_rps_played(interaction: Interaction) -> bool:
     """Verifies the event's activation status to make sure event can be played"""
-    if not bot_message or "I've made my choice." not in str(bot_message.content):
+    from bot import bot_message
+    if not bot_message or "chose" not in str(bot_message.content):
         return False
-    await interaction.response.send_message("Re-start the game!")
+    await interaction.response.send_message("`Re-start the game!`")
     return True
 
 
@@ -26,19 +27,18 @@ class RockPaperScissors(View):
     def __init__(self, bot_choice: str, bot_msg: Message):
         super().__init__(timeout=10)
         self.bot_choice = bot_choice
-        self.bot_msg = bot_msg #TODO currently has similar to old issue where it isn't the new one that is ran
 
     @button(label="Rock", row=0, style=ButtonStyle.red)
     async def rock(self, interaction: Interaction, Button: Button) -> None:
-        if not await check_rps_played(interaction, self.bot_msg):
+        if not await check_rps_played(interaction):
             await interaction.response.send_message(who_won_rps(self.bot_choice, "rock"))
 
     @button(label="Paper", row=0, style=ButtonStyle.red)
     async def paper(self, interaction: Interaction, Button: Button) -> None:
-        if not await check_rps_played(interaction, self.bot_msg):
+        if not await check_rps_played(interaction):
             await interaction.response.send_message(who_won_rps(self.bot_choice, "paper"))
 
     @button(label="Scissors", row=0, style=ButtonStyle.red)
     async def scissors(self, interaction: Interaction, Button: Button) -> None:
-        if not await check_rps_played(interaction, self.bot_msg):
+        if not await check_rps_played(interaction):
             await interaction.response.send_message(who_won_rps(self.bot_choice, "scissors"))
