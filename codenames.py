ede14c7c20a892b6b89234cfdf817f09be5bbd08
angelikavilde <angelikavilde@gmail.com>
2023-10-12 14:24:25 +0100

    finished all of the codenames commands to be changed to cog commands and cleaned up the file

--- codenames.py
+++ codenames.py
@@ -1,52 +1,93 @@
 """Codenames event bot functionalities"""
 
-import random
+from random import choice
 
-from events_help import help_documentation
+from discord.ext.commands import Cog, check, command
+from discord.ext.commands.context import Context
 
 
-def start_codenames(user_chose: str, user: str, events) -> str:
-    """CodeNames event function"""
+def is_codenames_event_activated():
+    """Predicate function to verify if command can be ran"""
+    async def predicate(*args):
+        from bot import servers_obj
+        """Returns True if the CodeNames event is activated on a server"""
+        return servers_obj.get_server().codenames_event
+    return check(predicate)
 
-    if user_chose[:4] == "//d ":
-        user = user_chose[4:]
-        user_chose = "//l"
 
-    elif user_chose == "//j":
-        if user in events.users_playing:
-            return f"`Cannot join! {user} has already joined the CodeNames event!`"
-        events.users_playing.append(user)
-        return f"`{user} was successfully added to the CodeNames event!`"
+class CodeNamesCog(Cog):
 
-    elif user_chose == "//l":
-        if user in events.users_playing:
-            events.users_playing.remove(user)
-            return f"`{user} successfully left CodeNames event!`"
-        else:
-            return f"`Cannot leave! {user} did not enter the game. '//j' to join!`"
+    def __init__(self, bot):
+        self.bot = bot
 
-    elif user_chose == "//t":
-        if len(events.users_playing) < 4:
+    @command(name="d")
+    @is_codenames_event_activated()
+    async def delete_user(self, ctx: Context, *args) -> None:
+        """Deletes a chosen player (not yourself) from an event"""
+        user = " ".join(args)
+        await ctx.send(remove_a_player(user))
+
+    @command(name="l")
+    @is_codenames_event_activated()
+    async def leave_event(self, ctx: Context) -> None:
+        """Removes a player from an event"""
+        user = str(ctx.author)
+        await ctx.send(remove_a_player(user))
+
+    @command(name="j")
+    @is_codenames_event_activated()
+    async def join_event(self, ctx: Context) -> None:
+        """Adds a player to the event"""
+        user = str(ctx.author)
+        await ctx.send(add_player(user))
+
+    @command(name="a")
+    @is_codenames_event_activated()
+    async def add_to_event(self, ctx: Context, *args) -> None:
+        """Adds another player (not yourself) to the event"""
+        user = " ".join(args)
+        await ctx.send(add_player(user))
+
+    @command(name="t")
+    @is_codenames_event_activated()
+    async def display_teams(self, ctx: Context) -> None:
+        """Displays random teams"""
+        from bot import servers_obj
+        if len(servers_obj.get_server().users_playing) < 4:
             return "`This game is designed for minimum of 4 people. Preferably 6 or more. Add more players!`"
-        team1 = events["users_playing"].copy()
+        team1 = servers_obj.get_server().users_playing.copy()
         team2 = []
-        for _ in range(int(len(events["users_playing"])/2)):
-            team2.append(team1.pop(team1.index(random.choice(team1))))
+        for _ in range(int(len(servers_obj.get_server().users_playing)/2)):
+            team2.append(team1.pop(team1.index(choice(team1))))
             team_1_formatted = ", ".join(team1)
             team_2_formatted = ", ".join(team2)
-        teams = f"""```Team 1: {team_1_formatted}; Captain: {random.choice(team1)}
-Team 2: {team_2_formatted}; Captain: {random.choice(team2)}```"""
+        teams = f"""```Team 1: {team_1_formatted}; Captain: {choice(team1)}
+Team 2: {team_2_formatted}; Captain: {choice(team2)}```"""
         return teams
 
-    elif user_chose == "//h":
-        return help_documentation("codenames")
-    
-    elif user_chose == "//sp":
-        users_playing = ", ".join(events.users_playing)
+    @command(name="show")
+    @is_codenames_event_activated()
+    async def show_current_players(self, ctx: Context) -> None:
+        """Displays current players to the chat"""
+        from bot import servers_obj
+        users_playing = ", ".join(servers_obj.get_server().users_playing)
         return f"`Currently playing are: {users_playing}`"
 
-    elif user_chose == "//q":
-        events.codenames_event = False
-        events.users_playing = []
-        return "`CodeNames event was ended!`"
-    return ""
+
+def remove_a_player(player: str) -> str:
+    """Removes a player from the code-names event"""
+    from bot import servers_obj
+    if player in servers_obj.get_server().users_playing:
+        servers_obj.get_server().users_playing.remove(player)
+        return f"`{player} successfully left CodeNames event!`"
+    else:
+        return f"`Cannot leave! {player} did not enter the game. '//j' to join!`"
+
+
+def add_player(player: str) -> str:
+    """Adds a player to the code-names event"""
+    from bot import servers_obj
+    if player in servers_obj.get_server().users_playing:
+        return f"`Cannot join! {player} has already joined the CodeNames event!`"
+    servers_obj.get_server().users_playing.append(player)
+    return f"`{player} was successfully added to the CodeNames event!`"
