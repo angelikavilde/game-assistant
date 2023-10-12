4129f71ce4a45123e663b5741261162e6a41264b
angelikavilde <angelikavilde@gmail.com>
2023-10-12 13:37:33 +0100

    added cog from the among us dead command

--- amongus.py
+++ amongus.py
@@ -1,47 +1,57 @@
 """Among-us event bot functionalities"""
 
 import discord
-from discord.message import Message
+from discord.ext.commands import Cog, check, command
+from discord.ext.commands.context import Context
 
-from events_help import help_documentation
 
-async def start_among_us(message: Message, user_chose: str, events) -> None:
-    """AmongUs event function"""
+def is_amongus_event_activated():
+    """Predicate function to verify if command can be ran"""
+    async def predicate(*args):
+        from bot import servers_obj
+        """Returns True if the AmongUs event is activated on a server"""
+        return servers_obj.get_server().amongus_event
+    return check(predicate)
 
-    if user_chose == "//d":
-        if not message.author in events.users_playing:
-            events.users_playing.append(message.author)
-            await member_role_changed(message, message.author, True)
-            await message.channel.send(f"`{str(message.author)} has been announced dead!`")
+
+class AmongUsCog(Cog):
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @command(name="d")
+    @is_amongus_event_activated()
+    async def player_died(self, ctx: Context) -> None:
+        """Changes the role of a player to a dead role if not already"""
+        from bot import servers_obj
+        if not ctx.author in servers_obj.get_server().users_playing:
+            servers_obj.get_server().users_playing.append(ctx.author)
+            await member_role_changed(ctx, ctx.author, True)
+            await ctx.send(f"`{str(ctx.author)} has been pronounced dead!`")
         else:
-            await message.channel.send(f"`{str(message.author)} is already laying in the grave!`")
-
-    elif user_chose == "//n":
-        for member in events.users_playing:
-            await member_role_changed(message, member, False)
-        events.users_playing = []
-        await message.channel.send("`AmongUs event was restarted!`")
-
-    elif user_chose == "//h":
-        await message.channel.send(help_documentation("amongus"))
-
-    elif user_chose == "//q":
-        for member in events.users_playing:
-            await member_role_changed(message, member, False)
-
-        events.amongus_event = False
-        events.users_playing = []
-        await message.channel.send("`AmongUs event was ended!`")
-    else:
-        await message.channel.send("This is not an allowed command for this event. Try '//h' for help!")
+            await ctx.send(f"`{str(ctx.author)} is already laying in the grave!`")
 
 
-async def member_role_changed(message: Message, user, add: bool) -> None:
+# if user_chose == "//n":
+#     for member in events.users_playing:
+#         await member_role_changed(message, member, False)
+#     events.users_playing = []
+#     await message.channel.send("`AmongUs event was restarted!`")
+
+
+async def clean_dead_roles(ctx: Context) -> None:
+    """Returns all players to a non dead crewmate role"""
+    from bot import servers_obj
+    for member in servers_obj.get_server().users_playing:
+        await member_role_changed(ctx, member, False)
+
+
+async def member_role_changed(ctx: Context, user: discord.member.Member, add: bool) -> None:
     """Gives a user a Dead Crewmate role or removes it"""
 
-    role = discord.utils.get(message.guild.roles, name="Dead Crewmate")
+    role = discord.utils.get(ctx.guild.roles, name="Dead Crewmate")
     if not role:
-        await message.channel.send("```Dead Crewmate role does not exist on this channel!```")
+        await ctx.send("```Dead Crewmate role does not exist on this channel!```")
     else:
         if add:
             await user.add_roles(role)
